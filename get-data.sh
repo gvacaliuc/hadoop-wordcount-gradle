6d892b05b85c9b4fd2eb3ef3830c9a653c133066
Gabriel Vacaliuc <gabe.vacaliuc@gmail.com>
2017-10-01 00:43:13 -0500

    added script to grab data

---
+++ get-data.sh
@@ -0,0 +1,4 @@
+wget https://s3.amazonaws.com/chrisjermainebucket/text/Holmes.txt
+wget https://s3.amazonaws.com/chrisjermainebucket/text/dictionary.txt
+wget https://s3.amazonaws.com/chrisjermainebucket/text/war.txt
+wget https://s3.amazonaws.com/chrisjermainebucket/text/william.txt
